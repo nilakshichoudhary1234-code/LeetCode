98127c70c8f85e00e1f413071df3e7a407621ae5
NILAKSHI CHOUDHARY <nilakshichoudhary1234@gmail.com>
2026-08-12 19:34:23 +0530

    Time: 3 ms (41.82%), Space: 8.6 MB (42.9%) - LeetHub

---
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    string sortSentence(string s) {
+        vector<string>ans(10);
+        string temp="";
+        int count=0,index=0;
+        while(index<s.size()){
+            if(s[index]==' '){
+                int pos = temp[temp.size()-1]-'0';
+                temp.pop_back();
+                ans[pos]=temp;
+                temp.clear();
+                count++;
+            }
+            else{
+                temp+=s[index];
+
+            }
+            index++;
+
+        }
+
+        if (!temp.empty()) {
+            int pos = temp.back() - '0';
+            temp.pop_back();
+            ans[pos] = temp;
+            count++;
+            temp.clear(); 
+        }
+        for(int i=1;i<=count;i++){
+            temp+=ans[i];
+            temp+=' ';
+
+        }
+        temp.pop_back();
+        return temp;
+
+        
+    }
+};

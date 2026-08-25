f61f356ea9b9df90504c2cc3066d2adb91a7318f
NILAKSHI CHOUDHARY <nilakshichoudhary1234@gmail.com>
2026-08-25 22:46:04 +0530

    Time: 7 ms (86.81%), Space: 14.1 MB (14.63%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -1,33 +1,36 @@
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <climits>
+
 class Solution {
 public:
-    int threeSumClosest(vector<int>& nums, int target) {
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
+    int threeSumClosest(std::vector<int>& nums, int target) {
+        std::sort(nums.begin(), nums.end());
         int closestSum = nums[0] + nums[1] + nums[2];
 
-        for (int i = 0; i < n - 2; ++i) {
+        for (int i = 0; i < nums.size() - 2; ++i) {
             int left = i + 1;
-            int right = n - 1;
+            int right = nums.size() - 1;
 
             while (left < right) {
                 int currentSum = nums[i] + nums[left] + nums[right];
-                
-                // If we find an exact match, return immediately
-                if (currentSum == target) return currentSum;
 
-                // Update closestSum if the current one is nearer to target
-                if (abs(target - currentSum) < abs(target - closestSum)) {
+                if (std::abs(target - currentSum) < std::abs(target - closestSum)) {
                     closestSum = currentSum;
                 }
 
-                // Move pointers based on comparison with target
                 if (currentSum < target) {
-                    left++;
+                    ++left;
+                } else if (currentSum > target) {
+                    --right;
                 } else {
-                    right--;
+                    return currentSum; // Found exact match
                 }
             }
         }
+
         return closestSum;
     }
 };
+
